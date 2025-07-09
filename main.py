80cc3d7b3002d6260ccff635fdedc068480d50d0
Robotics-IIITDM <148760410+Robotics-IIITDM@users.noreply.github.com>
2025-07-09 18:55:47 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from waitress import serve
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about_us():
+    return render_template("about.html")
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
+@app.route("/projects")
+def our_projects():
+    return render_template("projects.html")
+
+@app.route("/team")
+def team_data():
+    return render_template("team.html")
+
+@app.route("/contact")
+def contact_us():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    # Check if running on Render.com
+    if os.environ.get('RENDER'):
+        # Let Gunicorn handle the app
+        app.run()
+    else:
+        # Local development using Waitress
+        port = int(os.environ.get("PORT", 8080))
+        serve(app, host="0.0.0.0", port=port)
